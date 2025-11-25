0bcbee3273e2b0155bd585d8a820497b15061a48
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-11-25 10:58:33 +0000

    styles: Change styles from get api data home page

--- src/types/ApiDataType.ts
+++ src/types/ApiDataType.ts
@@ -1,6 +1,10 @@
 export type ApiDataType = {
   target_url: string;
   data: {
+    target: {
+      os: string,
+      technology: string
+    },
     injection_points: {
       type: string;
       title: string;
